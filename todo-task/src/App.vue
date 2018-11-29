3a091f45c57993c0ba5aaed575d1fe074eb060e5
didiaoyuwo <2108560699@qq.com>
2018-11-29 18:06:46 +0800

    todo

---
+++ todo-task/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style>
+*{
+  padding: 0;
+  margin: 0;
+}
+ul,li{
+  list-style: none;
+}
+#app{
+  margin-top:66px;
+}
+</style>
