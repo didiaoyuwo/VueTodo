b825dc7b4ad92cc97e22d130251b3d0d3e7fd681
didiaoyuwo <2108560699@qq.com>
2018-12-06 15:53:06 +0800

    edit

--- src/components/vueTodo.vue
+++ src/components/vueTodo.vue
@@ -11,19 +11,19 @@
     </header>
     <section class="todo-body">
       <ul>
-        <li v-for='(item,index) in items' :key='index' class='item-list' @mouseover='showDelete(index)' @mouseout="hideDelete(index)" v-if='allStatus || (ifIsCheck?item.isCheck:!item.isCheck)'>
+        <li v-for='(item,index) in items' :key='index' class='item-list' @mouseover='showDelete(item.id)' @mouseout="hideDelete(item.id)" v-show='allStatus || (ifIsCheck?item.isCheck:!item.isCheck)'>
           <input type="checkbox" class="item-checkbox" v-model='item.isCheck' v-if='item.hideInpStatus'>
-          <span class="item-msg" :class="{'gray':item.isCheck}" @dblclick="showInput(index)" v-if='!item.showInput' >{{item.val}}</span>
-          <input type="text" v-model="item.val" class="item-input" v-if='item.showInput' v-focus @keyup.enter='HideInput(index)' @blur='HideInput(index)'>
-          <img src="../../static/delete.png" alt="" class="delete-icon" v-if='item.showDelete && item.hideInpStatus' @click='deleteData(index)'>
+          <span class="item-msg" :class="{'gray':item.isCheck}" @dblclick="showInput(item.id)" v-if='!item.showInput' >{{item.val}}</span>
+          <input type="text" v-model="item.val" class="item-input" v-if='item.showInput' v-focus @keyup.enter='HideInput(item.id)' @blur='HideInput(item.id)'>
+          <img src="../../static/delete.png" alt="" class="delete-icon" v-if='item.showDelete && item.hideInpStatus' @click='deleteData(item.id)'>
         </li>
       </ul>
     </section>
     <footer class="todo-footer">
       <span>{{count}} item left</span>
-      <a class='footer-all' @click='showAll()' :class="{'active':allStatus}">all</a>
-      <a class='footer-active' @click='showActive()'>Active</a>
-      <a class='footer-completed' @click='showCompleted()'>Completed</a>
+      <a class='footer-all' @click='showAll($event)' :class="{'active':allStatus}">all</a>
+      <a class='footer-active' @click='showActive($event)'>Active</a>
+      <a class='footer-completed' @click='showCompleted($event)'>Completed</a>
       <a class='clear-completed' @click='clearCompleted' v-if='count < items.length'>clearCompleted</a>
     </footer>
   </section>
@@ -41,21 +41,21 @@ export default {
   computed: {
     count () {
       let count = 0
-      for (let i = 0; i < this.items.length; i++) {
-        if (this.items[i].isCheck === false) {
+      this.items.forEach((item) => {
+        if (item.isCheck === false) {
           count++
         }
-      }
+      })
       return count
     }
   },
   methods: {
     add (msg) {
-      // alert(1)
       if (msg === '') {
         alert('亲，您好像什么都没输出哦...')
       } else {
         this.items.push({
+          id: this.items.length,
           val: msg,
           isCheck: false,
           showInput: false,
@@ -64,59 +64,70 @@ export default {
           hideInpStatus: true
         })
       }
-      // localStorage.setItem('data', JSON.stringify(this.items))
       this.msg = ''
     },
-    showInput (index) {
-      this.items[index].showInput = true
-      this.items[index].hideInpStatus = false
+    showInput (id) {
+      this.items.forEach((item) => {
+        if (item.id === id) {
+          item.showInput = true
+          item.hideInpStatus = false
+        }
+      })
     },
-    HideInput (index) {
-      this.items[index].showInput = false
-      this.items[index].hideInpStatus = true
+    HideInput (id) {
+      this.items.forEach((item) => {
+        if (item.id === id) {
+          item.showInput = false
+          item.hideInpStatus = true
+        }
+      })
     },
-    showDelete (index) {
-      this.items[index].showDelete = true
+    showDelete (id) {
+      this.items.forEach((item) => {
+        item.showDelete = false
+        if (item.id === id) {
+          item.showDelete = true
+        }
+      })
     },
-    hideDelete (index) {
-      this.items[index].showDelete = false
+    hideDelete (id) {
+      this.items.forEach((item) => {
+        item.showDelete = false
+      })
     },
     selectAll () {
       let isAllChecked = true
-      for (let i = 0; i < this.items.length; i++) {
-        if (this.items[i].isCheck === false) {
+      this.items.forEach((item) => {
+        if (item.isCheck === false) {
           isAllChecked = false
-          break
         }
-      }
+      })
       if (isAllChecked) {
-        for (let j = 0; j < this.items.length; j++) {
-          this.items[j].isCheck = !this.items[j].isCheck
-        }
+        this.items.forEach((item) => {
+          item.isCheck = !item.isCheck
+        })
       } else {
-        for (let j = 0; j < this.items.length; j++) {
-          this.items[j].isCheck = true
-        }
+        this.items.forEach((item) => {
+          item.isCheck = true
+        })
       }
     },
     deleteData (index) {
-      // console.log(1)
       this.items.splice(index, 1)
     },
     clearCompleted () {
-      // alert(1)
       let templateData = []
-      for (let i = 0; i < this.items.length; i++) {
-        if (this.items[i].isCheck === false) {
-          templateData.push(this.items[i])
+      this.items.forEach((item) => {
+        if (item.isCheck === false) {
+          templateData.push(item)
         }
-      }
+      })
       this.items = templateData
     },
-    showCompleted () {
-      // alert(1)
+    showCompleted (event) {
       this.allStatus = false
       this.ifIsCheck = true
+
       let list = document.getElementsByTagName('a')
       for (let i = 0; i < list.length; i++) {
         list[i].classList.remove('active')
@@ -124,7 +135,7 @@ export default {
       const completed = list[2]
       completed.classList.add('active')
     },
-    showAll () {
+    showAll (event) {
       this.allStatus = true
       let list = document.getElementsByTagName('a')
       for (let i = 0; i < list.length; i++) {
@@ -132,10 +143,8 @@ export default {
       }
       const all = list[0]
       all.classList.add('active')
-      // console.log(list)
     },
-    showActive () {
-      // alert(1)
+    showActive (event) {
       this.allStatus = false
       this.ifIsCheck = false
       let list = document.getElementsByTagName('a')
@@ -145,19 +154,6 @@ export default {
       const active = list[1]
       active.classList.add('active')
     }
-  },
-  mounted () {
-    window.onbeforeunload = () => {
-      // return localStorage.setItem('store', JSON.stringify(this.items))
-      localStorage.clear()
-    }
-  },
-  created () {
-    let store = JSON.parse(localStorage.getItem('store'))
-    if (store) {
-      this.items = store
-    }
-    // console.log(store)
   }
 }
 </script>
